959875e138d412681fd04da7237845a1f544b66a
Michael Mulligan <mike@belineperspectives.com>
2021-11-16 17:15:01 -0500

    Update inquirer.sh
    License and no `bc`

--- dist/inquirer.sh
+++ dist/inquirer.sh
@@ -1,5 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+################## INQUIRER.SH ##################
+# https://github.com/kahkhang/Inquirer.sh
+
+# The MIT License (MIT)
+
+# Copyright (c) 2017
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 # store the current set options
 OLD_SET=$-
@@ -112,7 +136,7 @@ gen_index() {
   if [ $k -gt 0 ]; then
     for l in $(seq $k)
     do
-       echo "$l-1" | bc
+       expr $l - 1
     done
   fi
 }
@@ -639,3 +663,5 @@ text_input() {
 
   cleanup
 }
+
+################## INQUIRER.SH ##################
